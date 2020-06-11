e070869df26e13ce630bac0e1ccd193621d6b7f1
Renanvt <jonatan_rvs@hotmail.com>
2020-06-11 04:22:42 -0300

    Exercicio pronto Versao1

--- exe_unicesumar.c
+++ exe_unicesumar.c
@@ -3,8 +3,9 @@
 #include <string.h>
 #include <stdbool.h>
 
-int RA[5][2] = { {0,2},{0,0},{5,4},{4,7},{7,5} };
+char RA[] = "0200544775";
 bool verificarSenha();
+void limpaTela();
 int main(void){
   
   
@@ -17,43 +18,64 @@ Em caso do usuário não acertar as 5 dezenas, o processo é repetido infinitame
     //Meu RA: 20054477-5
     //Meu RA Convertido: 02-00-54-47-75
 
-    //char green[] = {0x1b,'[', '1',';','3','2','m',0};
-    char tranca[] = "0200544775";
-    int senha[1][2];
-    int i,j;
-    int tentativas=5;
-    bool acerto;
-    //printf(green,"Teste");
+    char senha[2];
+    int i=1,j=0;
+    int etapa=0;
+    bool acerto = false;
+    limpaTela();
+    printf("------------------\n");
     puts("ABERTURA DO COFRE");
-    puts("O código é formado por 9 digitos");
-    for(i=0;i<10;j++){
-        acerto=false;
-        printf("Digite a %d dezena formada por 2 digitos: \n",count+1);
-        scanf("%d%d",&senha[i][j]);
-        printf("%s",senha);
-        
-        if(verificarSenha(senha)){
-          printf("ETAPA %d DESBLOQUEADA\n",count+1);
+    puts("A abertura é formado\n por 5 etapas");
+    printf("------------------\n");
+    
+     getchar();
+    
+   
+    while(true){
+        limpaTela();
+       
+        printf("Digite a %d dezena formada por 2 digitos: \n",i);
+        scanf("%s",senha);
+       
+        if(verificarSenha(senha,etapa)==true){
+          printf("ETAPA %d DESBLOQUEADA\n", i);
+          i++;
+          etapa+=2;
           acerto=true;
+          getchar();
         }else{
           printf("SENHA ERRADA\n");
-          printf("VOÇÊ TEM, %d TENTATIVAS\n",--tentativas);
+          printf("REINICIANDO A TRANCA...\n");
+          i=1;
+          etapa=0;
           acerto=false;
-        }         
+          getchar();
+        }
+        putchar('\n');
+        getchar();
+        if(etapa==strlen(RA))
+          break;         
+        
     }
+    printf("COFRE ABERTO!\n");
     return 0;
 }
-bool verificarSenha(char senha[][]){
-  int count=0;
-  int etapa=0;
-  int i,j;
-  for(i=0;i<5;i++){
-    for(j=0;j<2;j++){
-      if(senha[i][j]==RA[i][j]){
-        return true;
+bool verificarSenha(char senha[],int etapa){
+  int i;
+  int acha = 0;
+  for(i=0;i<strlen(RA)-etapa;i++){ 
+      if(senha[i]==RA[i+etapa]){ 
+        acha+=1;
+      }else{
+        break;
       }
-    }
-  }
-  return false;
+    }      
+  if(acha==2)
+    return true;
+  else
+    return false;
 
+}
+void limpaTela(){
+  system("clear");
 }
